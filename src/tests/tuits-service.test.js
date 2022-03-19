eb2c0a6be1594fc28a264ca399e4430833eca5c2
Vaishnavishah <shahvaishnavi2411@gmail.com>
2022-03-19 19:42:24 -0400

    updated tests

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -69,27 +69,27 @@ describe('can delete tuit wtih REST API', () => {
 
 describe('can retrieve a tuit by their primary key with REST API', () => {
   // TODO: implement this
-    const tuitPosted = {
+    const tuit1 = {
         _id: '6201f769fa48a047ed68c081',
         tuit: 'sample tuit',
         //postedBy: user1._id,
     };
 
     beforeAll(async () => {
-        await deleteTuit(tuitPosted._id);
+        await deleteTuit(tuit1._id);
         await deleteUsersByUsername(user1.username);
     });
 
     afterAll(async () => {
-        await deleteTuit(tuitPosted._id);
+        await deleteTuit(tuit1._id);
         return await deleteUsersByUsername(user1.username);
     });
 
     test('can delete tuit with REST API', async () => {
         const userCreated = await createUser(user1);
-        const tuitCreated= await createTuit(userCreated._id, tuitPosted);
-        const retrievedTuit = await findTuitById(tuitPosted._id);
-        expect(retrievedTuit.tuit).toEqual(tuitPosted.tuit);
+        const tuitCreated= await createTuit(userCreated._id, tuit1);
+        const retrievedTuit = await findTuitById(tuit1._id);
+        expect(retrievedTuit.tuit).toEqual(tuit1.tuit);
         expect(retrievedTuit.postedBy).toEqual(userCreated._id);
     });
 
@@ -97,7 +97,7 @@ describe('can retrieve a tuit by their primary key with REST API', () => {
 
 describe('can retrieve all tuits with REST API', () => {
   // TODO: implement this
-    const tuitPosted = [
+    const tuitList = [
         {
             _id: '6201f769fa48a047ed68c081',
             tuit: 'sample tuit 1',
@@ -105,41 +105,39 @@ describe('can retrieve all tuits with REST API', () => {
         {
             _id: '6201f769fa48a047ed68c082',
             tuit: 'sample tuit 2',
-            //postedOn: '03/13/2022'
         },
         {
             _id: '6201f769fa48a047ed68c083',
             tuit: 'sample tuit 3',
-           // postedOn: '05/13.2022'
         }
     ];
 
     beforeAll(async () => {
         // remove user and tuits to delete in test
-        Promise.all(tuitPosted.map(async (tuit) => await deleteTuit(tuit._id)));
+        Promise.all(tuitList.map(async (tuit) => await deleteTuit(tuit._id)));
         return deleteUsersByUsername(user1.username);
     });
 
     // clean up after test runs
     afterAll(async () => {
         // remove any data we created
-        Promise.all(tuitPosted.map(async (tuit) => await deleteTuit(tuit._id)));
+        Promise.all(tuitList.map(async (tuit) => await deleteTuit(tuit._id)));
         return deleteUsersByUsername(user1.username);
     });
 
     test('can retrieve all tuits with REST API', async () => {
         const userCreated = await createUser(user1);
-        await Promise.all(tuitPosted.map(async (tuit) => {
+        await Promise.all(tuitList.map(async (tuit) => {
             return await createTuit(userCreated._id, tuit);
         }));
 
         const retrievedTuits = await findAllTuits();
-        expect(retrievedTuits.length).toBeGreaterThanOrEqual(tuitPosted.length);
+        expect(retrievedTuits.length).toBeGreaterThanOrEqual(tuitList.length);
         const tuitsWeInserted = retrievedTuits.filter(
             tuit => tuit.postedBy === userCreated._id);
 
         tuitsWeInserted.forEach(tuitInserted => {
-            const tuit = tuitPosted.find(tuit => tuit._id === tuitInserted._id);
+            const tuit = tuitList.find(tuit => tuit._id === tuitInserted._id);
             expect(tuitInserted.tuit).toEqual(tuit.tuit);
             expect(tuitInserted.postedBy).toEqual(userCreated._id);
         });
