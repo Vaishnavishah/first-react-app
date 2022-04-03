22df4b258c8b82e2595f979457bbced6e7892f32
Vaishnavishah <shahvaishnavi2411@gmail.com>
2022-04-02 21:14:30 -0400

    Documentation added

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -2,6 +2,11 @@ import Tuits from "../tuits";
 import * as service from "../../services/likes-service";
 import {useEffect, useState} from "react";
 
+/**
+ * My dislikes component is used to find all the tuits that are disliked by the user.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const MyDislikes = () => {
     const [dislikedTuits, setDislikedTuis] = useState([]);
     const findTuitsIDislike = () =>
--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -8,18 +8,31 @@ const api = axios.create({
   withCredentials: true
 });
 
+/**
+ * A service to find all the tuits which are liked by the user which uses get request to fetch the response.
+ * @param userId user id
+ * @returns {Promise<AxiosResponse<any>>} tuits object
+ */
 export const findAllTuitsLikedByUser = (userId) =>
     api.get(`${USERS_API}/${userId}/likes`)
         .then(response => response.data);
 
+/**
+ * A service to find all the users that liked the tuit which uses get request to fetch the response.
+ * @param tid tuit id
+ * @returns {Promise<AxiosResponse<any>>} users object
+ */
 export const findAllUsersThatLikedTuit = (tid) =>
     api.get(`${TUITS_API}/${tid}/likes`)
         .then(response => response.data);
 
-// export const userLikesTuit = (uid, tid) =>
-//     api.put(`${USERS_API}/${uid}/likes/${tid}`)
-//         .then(response => response.data);
 
+/**
+ * A service to like the tuit by a particular user  which uses put to update the database.
+ * @param uid user id
+ * @param tid tuit id
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 export const userTogglesTuitLikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
         .then(response => response.data);
@@ -27,13 +40,23 @@ export const userTogglesTuitLikes = (uid, tid) =>
 export const userTogglesTuitUnlikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/unlikes/${tid}`)
         .then(response => response.data);
-
+/**
+ * A service to dislike the tuit by a user.
+ * @param uid user id
+ * @param tid tuit id
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 export const userTogglesTuitDislikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data).catch(e => {
           console.log("here: " + e);
     });
 
+/**
+ * A service to find all the tuits which are disliked by the user.
+ * @param userId user id
+ * @returns {Promise<AxiosResponse<any>>} tuits object
+ */
 export const findAllTuitsDislikedByUser = (userId) =>
     api.get(`${USERS_API}/${userId}/dislikes`)
         .then(response => response.data);
